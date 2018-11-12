9490fbc4b036f46c1eb3cafe6f99a5e90692d47a
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2018-11-12 13:48:31 +0000

    restarted

---
+++ Assets/CatController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    private Animator anim;
+    private float moveSpeed = 0;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate ()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        if (moveSpeed != 0 && vertical == 0)
+        {
+            moveSpeed *= 0.9f; //damping
+        }
+        else
+        {
+            moveSpeed = vertical;
+        }
+
+        if (moveSpeed > 0)
+        {
+            anim.SetFloat("speed", moveSpeed);
+        }
+        
+	}
+}
